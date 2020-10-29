0db8e01584c987af8f46e994bee130e1167af417
Wayne Zhu <way87767@gmail.com>
2020-10-29 12:57:51 -0400

    added platform specific warnings

--- c1-cli.js
+++ c1-cli.js
@@ -13,7 +13,16 @@ program
   .parse(process.argv)
 
 if(process.argv.length <= 2){
-  console.log(chalk.bgRed.bold("Error: no file specified. Use c1-cli-win.exe -h for help."));
+  let platform = process.platform;
+  switch(platform){
+    case "darwin":
+      platform = "macos";
+      break;
+    case "win32":
+      platform = "win.exe";
+      break;
+  }
+  console.log(chalk.bgRed.bold("Error: no file specified. Use c1-cli-" + platform + "-h for help."));
 }
 else{
   let filePath = process.argv[process.argv.length - 1];
